8470f33723ab272272239a67429fbd858df66e85
tangzhihe <18326672274@163.com>
2017-08-04 09:14:03 +0800

    commit code

---
+++ src/test/demo04/A.java
@@ -0,0 +1,5 @@
+package test.demo04;
+
+public class A {
+	
+}
---
+++ src/test/demo04/Test.java
@@ -0,0 +1,33 @@
+package test.demo04;
+
+public class Test {
+	static int x = 10;
+	static {
+		x += 5;
+	}
+	public static void main(String[] args){
+		System.out.println("x = " + x);
+	}
+	
+	static {
+		x /= 3;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
