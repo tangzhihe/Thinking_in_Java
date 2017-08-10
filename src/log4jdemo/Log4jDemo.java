f501eb11c7cd0d07be0ead14bddcccdc97818379
tangzhihe <18326672274@163.com>
2017-08-10 20:42:22 +0800

    commit code

--- src/log4jdemo/Log4jDemo.java
+++ src/log4jdemo/Log4jDemo.java
@@ -5,8 +5,11 @@ import org.apache.log4j.Logger;
 public class Log4jDemo {
 	public static void main(String[] args){
 		Logger logger = Logger.getLogger(log4jdemo.Log4jDemo.class);
-		logger.debug("debug");
+		logger.info("----------------------LOG4j info start--------------------------");
 		logger.info("info");
-		logger.error("error");
+		logger.info("----------------------LOG4j info end----------------------------");
+		logger.info("----------------------LOG4j error start--------------------------");
+		logger.info("error");
+		logger.info("----------------------LOG4j error end----------------------------");
 	}
 }
